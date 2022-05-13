de6851ffa20dd62f5f3c348867ebafa5a0da7f55
davidlopez232 <diedlopez1@misena.edu.co>
2022-05-13 12:13:18 -0500

    El inicio del Fin

---
+++ principal/views.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.shortcuts import render
+from .models import Departamento
+from django.conf import settings
+from django.core.mail import send_mail
+
+def inicio(request):
+    Departamentos = Departamento.objects.all()
+    contexto = {
+        'Departamentos':Departamentos
+    }
+    return render(request, 'index.html', contexto)
+
+def formularioContacto(request):
+    return render(request, 'formularioContacto.html')
+
+def contactar(request):
+    if request.method == "POST":
+        asunto = request.POST ["txtAsunto"]
+        mensaje = request.POST ["txtMensaje"] + " / Email: " + request.POST["txtEmail"]
+        email_desde = settings.EMAIL_HOST_USER
+        email_para = ["kasekage233@gmail.com"]
+        send_mail(asunto, mensaje, email_desde, email_para, fail_silently=False)
+        return render(request, "contactoExitoso.html")
+    return render(request, "formularioContato.html")
